bfc690d2f7cb967cd8155aab1d09f21e67ef0b54
Linda ONeill <oneill.linda21@gmail.com>
2017-11-13 22:53:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,4 +11,7 @@ function forLoop(array) {
     return array;
 }
 function whileLoop(n) {let countdown = n; while (countdown > 0) {console.log(--countdown)}; return "done"}
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 function doWhileLoop(array) {do [array.shift] while ([array.length > 0 && maybeTrue()]); return array}
