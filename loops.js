cb4160c6ce538c24912a4ee65bf327336733a635
Linda ONeill <oneill.linda21@gmail.com>
2017-11-13 23:53:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,5 +11,12 @@ function forLoop(array) {
     return array;
 }
 function whileLoop(n) {let countdown = n; while (countdown > 0) {console.log(--countdown)}; return "done"}
+function maybeTrue() {{
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());}
 function doWhileLoop(array) {do [array.length - 1];
 } while ([array.length > 0 && maybeTrue()]); return array}
