fcc9035d94a7d607df171e690508d44b6366b7a6
Linda ONeill <oneill.linda21@gmail.com>
2017-11-13 22:43:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am " + i + " strange loop.");
+    }
+      else {
+        array.push("I am ${i} strange loops.");
+      }
+
+    }
+    return array;
+}
+function whileLoop(n) {let countdown = n; while (countdown > 0) {return console.log("done")}}
